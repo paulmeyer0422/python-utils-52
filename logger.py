3c552b5b1738fd17e033d412056484ea4fdc6299
Viktor Lindqvist <paulmeyer0422@gmail.com>
2026-08-11 04:15:47 +0200

    update performance improvements in logging

--- logger.py
+++ logger.py
@@ -1,48 +1,31 @@
 import logging
-from typing import Optional
 
-class Logger:
-    """
-    A simple logging utility.
-    """
-
-    def __init__(self, name: str, level: Optional[int] = logging.INFO) -> None:
-        """
-        Initializes the logger with the given name and level.
-        """
+class CustomLogger:
+    def __init__(self, name):
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
+        self.logger.setLevel(logging.DEBUG)
         handler = logging.StreamHandler()
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
 
-    def debug(self, message: str) -> None:
-        """
-        Logs a message with level DEBUG.
-        """
-        self.logger.debug(message)
+    def debug(self, msg):
+        self.logger.debug(msg)
 
-    def info(self, message: str) -> None:
-        """
-        Logs a message with level INFO.
-        """
-        self.logger.info(message)
+    def info(self, msg):
+        self.logger.info(msg)
 
-    def warning(self, message: str) -> None:
-        """
-        Logs a message with level WARNING.
-        """
-        self.logger.warning(message)
+    def warning(self, msg):
+        self.logger.warning(msg)
 
-    def error(self, message: str) -> None:
-        """
-        Logs a message with level ERROR.
-        """
-        self.logger.error(message)
+    def error(self, msg):
+        self.logger.error(msg)
 
-    def critical(self, message: str) -> None:
-        """
-        Logs a message with level CRITICAL.
-        """
-        self.logger.critical(message)
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+# Example of usage
+if __name__ == '__main__':
+    log = CustomLogger(__name__)
+    log.info('This is an info message')
+    log.error('This is an error message')
