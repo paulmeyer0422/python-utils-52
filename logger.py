6be2b1b08c1640c27df442a7eec7abb1d03d69a1
Viktor Lindqvist <paulmeyer0422@gmail.com>
2026-08-13 09:16:49 +0200

    logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,31 +1,15 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-class CustomLogger:
-    def __init__(self, name):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+def setup_logger(log_file, max_bytes=5 * 1024 * 1024, backup_count=3):
+    logger = logging.getLogger('AutoClickerLogger')
+    logger.setLevel(logging.DEBUG)
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
 
-    def debug(self, msg):
-        self.logger.debug(msg)
-
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
-
-# Example of usage
 if __name__ == '__main__':
-    log = CustomLogger(__name__)
-    log.info('This is an info message')
-    log.error('This is an error message')
+    logger = setup_logger('autoclicker.log')
+    logger.info('Logger setup complete.')
