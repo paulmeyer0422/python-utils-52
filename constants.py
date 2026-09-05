a72e8987c0585b055e27b9960bb2ea1fd4177ae4
Viktor Lindqvist <paulmeyer0422@gmail.com>
2026-09-05 09:06:24 +0200

    add definition of autoclicker constants and schemas

--- constants.py
+++ constants.py
@@ -1,48 +1,33 @@
-import random
-DEFAULT_INTERVAL = 0.05
-MAX_CLICKS = 10000
-MIN_INTERVAL = 0.001
-BUTTONS = {
-    "left": "left",
-    "right": "right",
-    "middle": "middle"
+import enum
+from typing import Final
+
+class ClickType(enum.Enum):
+    LEFT = 'left'
+    RIGHT = 'right'
+    MIDDLE = 'middle'
+
+class ActionState(enum.Enum):
+    IDLE = 0
+    RUNNING = 1
+    PAUSED = 2
+
+DEFAULT_INTERVAL: Final[float] = 0.1
+MAX_CLICK_RATE: Final[float] = 1000.0
+CONFIG_FILE: Final[str] = "config.json"
+
+SETTINGS_SCHEMA: Final[dict] = {
+    "interval": float,
+    "button": str,
+    "repeat": int,
+    "hotkey": str
 }
-KEYBOARD_KEYS = {
-    "start": "f8",
-    "pause": "f9",
-    "stop": "f10"
-}
-DEFAULT_POSITION = (0, 0)
-SCREEN_RESOLUTION = (1920, 1080)
-DELAY_TYPES = ["random", "fixed", "increasing"]
-RANDOM_DELAY_MIN = 0.01
-RANDOM_DELAY_MAX = 0.2
 
-def calculate_delay(base_delay, variation):
-    return base_delay + random.uniform(-variation, variation)
+EXIT_KEYS: Final[list[str]] = ["esc", "f12"]
 
-def get_button_from_string(btn_str):
-    btn_str = btn_str.lower()
-    if btn_str == "left":
-        return "left"
-    if btn_str == "right":
-        return "right"
-    if btn_str == "middle":
-        return "middle"
-    return "left"
-
-def validate_interval(interval):
-    return max(MIN_INTERVAL, min(interval, 1.0))
-
-def get_hotkey(action):
-    return KEYBOARD_KEYS.get(action)
-
-def get_random_position():
-    return (random.randint(0, SCREEN_RESOLUTION[0]), random.randint(0, SCREEN_RESOLUTION[1]))
-
-def apply_delay_type(delay, delay_type, click_count):
-    if delay_type == "random":
-        return calculate_delay(delay, 0.05)
-    elif delay_type == "increasing":
-        return delay + (click_count * 0.001)
-    return delay
+def get_default_config() -> dict:
+    return {
+        "interval": DEFAULT_INTERVAL,
+        "button": ClickType.LEFT.value,
+        "repeat": -1,
+        "hotkey": "f9"
+    }
